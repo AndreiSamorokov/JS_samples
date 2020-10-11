7ee8fb4d8922a87afd3eda03d6502509b933084f
AndreiSamorokov <andreiv0202@gmail.com>
2020-10-12 05:25:11 +0900

    1st file

---
+++ primitiveVSreference.js
@@ -0,0 +1,29 @@
+
+var date = "2020-10-12";
+var datavalue = new Date(date);
+
+console.log( datavalue.getDay() );
+
+
+
+var name = "max";
+console.log(name);
+
+var second_name = name;
+console.log(second_name);
+
+name = "Antonella"
+console.log( second_name );
+
+var person = {
+  age: 25,
+  name: "Max"
+};
+
+console.log( person );
+
+var secondperson = person;
+
+person.name = 'Antonella';
+console.log( secondperson );
+
